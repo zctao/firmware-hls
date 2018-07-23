14fcc6610e0b366a02364e2a621216b11ebb9969
Peter Wittich <wittich@cornell.edu>
2018-07-23 16:51:51 -0400

    printing helper functions for full stub and reduced stub classes

--- TrackletAlgorithm/HelperFunctions.cpp
+++ TrackletAlgorithm/HelperFunctions.cpp
@@ -3,11 +3,49 @@
 #include <ostream>
 
 #include "FullStubLayer2S.hh"
+#include "FullStubLayerPS.hh"
+#include "ReducedStubLayer.hh"
 
 std::ostream & operator<<(std::ostream & o, const FullStubLayer2S & sl )
 {
-	o << "incomplete cout implementation" << std::endl;
-	return o;
+  o << "FullStubLayer2S: " 
+    << "raw data: 0x" << std::hex << sl.raw() << std::dec
+    << ", pt, r, phi, z " 
+    << sl.GetPt() << ", " << sl.GetR() << ", "
+    << sl.GetPhi() << ", " << sl.GetZ() 
+    << std::endl;
+  
+  return o;
 
 }
+
+
+std::ostream & operator<<(std::ostream & o, const FullStubLayerPS & sl )
+{
+  o << "FullStubLayerPS: " 
+    << "raw data: 0x" << std::hex << sl.raw() << std::dec
+    << ", pt, r, phi, z " 
+    << sl.GetPt() << ", " << sl.GetR() << ", "
+    << sl.GetPhi() << ", " << sl.GetZ() 
+    << std::endl;
+  
+  return o;
+
+}
+
+std::ostream & operator<<(std::ostream & o, const ReducedStubLayer & sl )
+{
+  o << "ReducedStubLayer: " 
+    << "raw data: 0x" << std::hex << sl.raw() << std::dec
+    << ", pt, r, phi, z, index: " 
+    << sl.GetPt() << ", " << sl.GetR() << ", "
+    << sl.GetPhi() << ", " << sl.GetZ() << ", "
+    << sl.GetIndex()
+    << std::endl;
+  
+  return o;
+
+}
+
+
 #endif // not __SYNTHESIS__
