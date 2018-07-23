93114de2b682225714f1b19a98dbd3947d8c4519
Peter Wittich <wittich@cornell.edu>
2018-07-23 13:46:35 -0400

    helper functions

---
+++ TrackletAlgorithm/HelperFunctions.cpp
@@ -0,0 +1,13 @@
+
+#ifndef __SYNTHESIS__
+#include <ostream>
+
+#include "FullStubLayer2S.hh"
+
+std::ostream & operator<<(std::ostream & o, const FullStubLayer2S & sl )
+{
+	o << "incomplete cout implementation" << std::endl;
+	return o;
+
+}
+#endif // not __SYNTHESIS__
