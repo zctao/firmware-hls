b0e41a08a8ad028c5728b4d792028facc7102e7f
Peter Wittich <wittich@cornell.edu>
2018-10-04 11:57:40 -0400

    const -> constexpr

--- TrackletAlgorithm/Constants.hh
+++ TrackletAlgorithm/Constants.hh
@@ -5,16 +5,16 @@
 using namespace std;
 
 // Define max number of stubs an individual module can take
-const int MAX_nSTUBS = 64;
-const int MAX_nSECTORS = 1;
-const int MAX_nINNERRS = 3;
-const int MAX_nOUTERRS = 3;
-const int MAX_nREGIONS = 6;
-const int MAX_nEVENTS = 100;
+constexpr int MAX_nSTUBS = 64;
+constexpr int MAX_nSECTORS = 1;
+constexpr int MAX_nINNERRS = 3;
+constexpr int MAX_nOUTERRS = 3;
+constexpr int MAX_nREGIONS = 6;
+constexpr int MAX_nEVENTS = 100;
 
 // Define list of detector regions to run algorithm over
 const string regionList[MAX_nREGIONS] = {"L1D3","L2D3","L3D3","L4D3","L5D3","L6D3"};
-const int nroutes[MAX_nREGIONS] = {6,8,6,8,6,8};
+constexpr int nroutes[MAX_nREGIONS] = {6,8,6,8,6,8};
 
 // Raw Stub Data
 typedef ap_uint<36> StubData;
