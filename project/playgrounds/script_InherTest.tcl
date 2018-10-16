a1011bb1ded1203d72110e26dd860f56328bdacc
zctao <zt75@cornell.edu>
2018-10-16 09:24:42 -0400

    Remove '-std=c++11' cflags because it seems to cause problems for cosimulation (Vivado HLS 2018.2 @ lnx231.classe.cornell.edu)

--- project/playgrounds/script_InherTest.tcl
+++ project/playgrounds/script_InherTest.tcl
@@ -1,7 +1,7 @@
 open_project -reset InheritanceTest
 set_top InheritanceTest
 add_files InheritanceTest/InheritanceTest.cc
-add_files -tb -cflags "-std=c++11"  InheritanceTest/InheritanceTest_tb.cc
+add_files -tb InheritanceTest/InheritanceTest_tb.cc
 open_solution -reset "solution1"
 set_part {xcku115-flvb2104-2-e} -tool vivado
 create_clock -period 4 -name default
