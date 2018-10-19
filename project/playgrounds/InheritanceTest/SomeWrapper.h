7b9cb2e749a680665339026def8a0806a238eecb
zctao <zt75@cornell.edu>
2018-10-19 09:00:00 -0400

    Fix

--- project/playgrounds/InheritanceTest/SomeWrapper.h
+++ project/playgrounds/InheritanceTest/SomeWrapper.h
@@ -2,12 +2,12 @@
 #define SOMEWRAPPER
 
 void SomeWrapper (ProcessBase * aProc,
-				  int (&inputmem1)[32], int (&inputmem2)[32],
-				  int (&inputmem3)[32], int (&inputmem4)[32],
-				  int (&inputmem5)[32], int (&inputmem6)[32],
-				  int (&inputmem7)[32], int (&inputmem8)[32],
-				  ap_uint<5> (&numbersin)[8],
-				  int (&outputmem1)[32], int (&outputmem2)[32])
+				  int inputmem1[32], int inputmem2[32],
+				  int inputmem3[32], int inputmem4[32],
+				  int inputmem5[32], int inputmem6[32],
+				  int inputmem7[32], int inputmem8[32],
+				  ap_uint<5> numbersin[8],
+				  int outputmem1[32], int outputmem2[32])
 {
 	aProc->process_virtual(inputmem1, inputmem2, inputmem3, inputmem4,
 						   inputmem5, inputmem6, inputmem7, inputmem8,
