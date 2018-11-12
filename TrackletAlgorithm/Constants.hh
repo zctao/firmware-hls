934ec6ee98a03120b1aadd57dc2f610a9d1fa2c5
Peter Wittich <wittich@cornell.edu>
2018-11-11 21:22:26 -0500

    Rationalize wrt master by hand for merge

--- TrackletAlgorithm/Constants.hh
+++ TrackletAlgorithm/Constants.hh
@@ -2,7 +2,6 @@
 #pragma once
 #include "ap_int.h"
 
-using namespace std;
 
 // Define max number of stubs an individual module can take
 constexpr int MAX_nSTUBS = 64;
@@ -12,29 +11,9 @@ constexpr int MAX_nOUTERRS = 3;
 constexpr int MAX_nREGIONS = 6;
 constexpr int MAX_nEVENTS = 100;
 
-// Define list of detector regions to run algorithm over
-const string regionList[MAX_nREGIONS] = {"L1D3","L2D3","L3D3","L4D3","L5D3","L6D3"};
-constexpr int nroutes[MAX_nREGIONS] = {6,8,6,8,6,8};
-
 // Raw Stub Data
 typedef ap_uint<36> StubData;
-typedef ap_uint<18> ReducedStubData;
 
-// Define bit widths for full stub parameters (for both PS & 2S)
-typedef ap_uint<12> FullZ_Layer_PS;
-typedef ap_uint<14> FullPhi_Layer_PS;
-typedef ap_uint<7>  FullR_Layer_PS;
-typedef ap_uint<3>  FullPt_Layer_PS;
 
-typedef ap_uint<8> FullZ_Layer_2S;
-typedef ap_uint<17> FullPhi_Layer_2S;
-typedef ap_uint<8>  FullR_Layer_2S;
-typedef ap_uint<3>  FullPt_Layer_2S;
 
-// Define bit widths for reduced stub parameters
-typedef ap_uint<4> ReducedZ_Layer;
-typedef ap_uint<3> ReducedPhi_Layer;
-typedef ap_uint<2> ReducedR_Layer;
-typedef ap_uint<3> ReducedPt_Layer;
-typedef ap_uint<6> ReducedIndex;
 
